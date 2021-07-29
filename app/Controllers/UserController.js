1bfa4586d39c9d6ed0c3b55e95847cfbf07312bb
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-29 15:13:10 -0300

    Testes em postman adicionado

--- app/Controllers/UserController.js
+++ app/Controllers/UserController.js
@@ -4,29 +4,37 @@
 
 /* CREATE */
 exports.post = (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    return res.json([name, email]);
     /* Status de retorno com sucesso. */
-    res.status(201).send('Rota POST!');
+    /* res.status(201).send('Rota POST!'); */
 };
 
 /* READ */
 exports.get = (req, res, next) => {
-    res.status(200).send('Rota GET!');
+    const result = { name: 'emerson', instagram: 'emersonbrogadev' };
+    return res.json(result);
+    /* res.status(200).send('Rota GET!'); */
 };
 
 /* READ BY ID */
 exports.getbyId = (req, res, next) => {
     let id = req.params.id;
-    res.status(200).send('Rota GET com ID! ${id}');
+    return res.json(id);
+    /* res.status(200).send('Rota GET com ID! ${id}'); */
 };
 
 /* UPDATE */
 exports.put = (req, res, next) => {
     let id = req.params.id;
-    res.status(201).send('Rota PUT com ID!" --> ${id}');
+    return res.json([id]);
+    /* res.status(201).send('Rota PUT com ID!" --> ${id}'); */
 };
 
 /* DELETE */
 exports.delete = (req, res, next) => {
     let id = req.params.id;
+    return res.json([id]);
     res.status(200).send('Rota DELETE com ID! --> ${id}');
 };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /* Configuração que permite aceitar requisições
  de qualquer origem com o cors e retorna tudo em json. */
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true}));
 require('./app/Routes/index')(app);
 
 app.use(cors());
