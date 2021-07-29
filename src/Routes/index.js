69d5de002bd119b12e6f3aa75574db41acceefad
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-29 10:29:58 -0300

    Route index adicionado

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,11 @@
 /* É necessáro Routes para acessar os controladores. */
 
 /* Juntando todas as rotas */
-const UsuarioRoute = require('./UserRoute');
+
+/* Variável constante "UserRoute" que como exigência, nos permite
+acessar o "UserRoutes e sua rotas" */
+const UserRoute = require('./UserRoute');
 
 module.exports = (app) => {
-    UsuarioRoute(app)
+    UserRoute(app)
 }
